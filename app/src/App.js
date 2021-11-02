cdeb296f4e6ae74149aa8247383dc91e1ae6c4cc
Louis Tamanini <louis.tamanini@gmail.com>
2021-11-01 20:57:45 -0400

    Setup the redux store and store the feature collection in it (#4)

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,29 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import './App.css';
 
 import Map from './components/Map';
-
-
+import observationsList from './components/observations.json';
+import { INITIALIZE_DATA } from './store/types';
 
 function App() {
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    const featureCollection = observationsList.features.map(
+      (feature, index) => {
+        return {
+          ...feature,
+          properties: {
+            ...feature.properties,
+            id: `ghg-obs-${index}`,
+          },
+        };
+      }
+    );
+
+    dispatch({ type: INITIALIZE_DATA, payload: featureCollection }); // send collection to the store
+  }, [dispatch]);
 
   return (
     <div>
---
+++ app/src/store/types.js
@@ -0,0 +1,1 @@
+export const INITIALIZE_DATA = 'initialize_data';
